002e87856bf7a9924e23e081177a20a09a1ba153
thgee <102576089+thgee@users.noreply.github.com>
2025-01-04 00:30:51 +0900

    feat: action-btn의 type이 `add` 일 때만 반응형 UI 구현

--- components/action-btn.tsx
+++ components/action-btn.tsx
@@ -16,7 +16,7 @@ const ICONS = {
 
 // 버튼 타입별 스타일 정의
 const BUTTON_STYLES = {
-  add: "bg-violet-600 hover:bg-violet-700 text-white disabled:bg-slate-200 disabled:text-slate-900",
+  add: "w-[54.78px] tablet:w-[164.35px] bg-violet-600 hover:bg-violet-700 text-white disabled:bg-slate-200 disabled:text-slate-900",
   delete: "bg-rose-500 text-white hover:bg-rose-600",
   edit: "bg-lime-300 hover:bg-lime-400 text-slate-900 disabled:bg-slate-200 disabled:text-slate-900",
 };
@@ -37,7 +37,7 @@ interface ActionBtnProps {
 }
 
 // 타입별로 재사용 가능한 버튼
-// 480px을 기준으로 UI가 변경되도록 반응형 구현
+// add 타입의 경우 480px을 기준으로 UI가 변경되도록 반응형 구현
 const ActionBtn = ({ type, active = false, onClick }: ActionBtnProps) => {
   // 버튼 타입과 활성화 상태에 따라 적절한 아이콘 선택
   const icon =
@@ -51,8 +51,10 @@ const ActionBtn = ({ type, active = false, onClick }: ActionBtnProps) => {
       disabled={!active} // 비활성 상태인 경우 버튼 비활성화
     >
       <Image src={icon} alt={`${type}Icon`} />
-      {/* 모바일 환경에서는 텍스트 숨기기*/}
-      <span className="tablet:block hidden">{BUTTON_TEXTS[type]}</span>
+      {/* type이 add일 때만 모바일 환경에서 텍스트 숨기기*/}
+      <span className={`tablet:block ${type === "add" ? "hidden" : ""}`}>
+        {BUTTON_TEXTS[type]}
+      </span>
     </button>
   );
 };
