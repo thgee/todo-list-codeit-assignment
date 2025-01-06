5ba0ad553d90a5ae45255542b0f44c6b71aa5c6b
thgee <102576089+thgee@users.noreply.github.com>
2025-01-06 21:09:04 +0900

    feat: `add-input` 컴포넌트에 `action` 버튼 통합

--- components/add-input.tsx
+++ components/add-input.tsx
@@ -1,10 +1,16 @@
+import ActionBtn from "./action-btn";
+
 const AddInput = () => {
   return (
-    <input
-      type="text"
-      placeholder="할 일을 입력해주세요"
-      className="h-[52.5px] w-full rounded-[24px] border-[2px] border-slate-900 p-[24px_17px] font-nanumsquare-regular text-16px shadow-2px-slate-900 outline-none placeholder:text-slate-500"
-    />
+    <section className="mb-[24px] flex gap-[8px] tablet:mb-[40px] tablet:gap-[16px]">
+      <input
+        type="text"
+        placeholder="할 일을 입력해주세요"
+        className="h-[52.5px] w-full rounded-[24px] border-[2px] border-slate-900 p-[24px_17px] font-nanumsquare-regular text-16px shadow-2px-slate-900 outline-none placeholder:text-slate-500"
+      />
+
+      <ActionBtn onClick={() => {}} type="add" />
+    </section>
   );
 };
 
