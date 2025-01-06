ee3f0a5778cfc14ca9b8251133f027589e4f4f7c
thgee <102576089+thgee@users.noreply.github.com>
2025-01-06 13:30:03 +0900

    style: edit-img-btn 컴포넌트 UI 구현

---
+++ components/edit-img-btn.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import addIcon from "../public/icons/plus-gray.svg";
+import editIcon from "../public/icons/edit.svg";
+
+const BUTTON_STYLE = {
+  add: "bg-slate-200",
+  edit: "border-slate-900 border-2 bg-slate-900 bg-opacity-50",
+};
+
+interface EditImgBtnProps {
+  type: "add" | "edit";
+  onClick: () => void;
+}
+
+const EditImgBtn = ({ type, onClick }: EditImgBtnProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex size-[64px] items-center justify-center rounded-full ${BUTTON_STYLE[type]}`}
+    >
+      <Image src={type === "add" ? addIcon : editIcon} alt={`${type}Icon`} />
+    </button>
+  );
+};
+
+export default EditImgBtn;
