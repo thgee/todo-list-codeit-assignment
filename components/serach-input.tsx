7b21c8f462fd95479f63c0978e0d3b186e7586af
thgee <102576089+thgee@users.noreply.github.com>
2025-01-06 11:47:38 +0900

    feat: search-input 컴포넌트 구현

---
+++ components/serach-input.tsx
@@ -0,0 +1,11 @@
+const SerachInput = () => {
+  return (
+    <input
+      type="text"
+      placeholder="할 일을 입력해주세요"
+      className="h-[52.5px] w-full rounded-[24px] border-[2px] border-slate-900 p-[24px_17px] font-nanumsquare-regular text-16px shadow-2px-slate-900 outline-none placeholder:text-slate-500"
+    />
+  );
+};
+
+export default SerachInput;
