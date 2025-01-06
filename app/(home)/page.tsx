cb7a3198ae0322b7a33e4caa9b15c6c066488293
thgee <102576089+thgee@users.noreply.github.com>
2025-01-06 21:13:59 +0900

    feat: `home 페이지` 에 `getItems` API 연동

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,37 +1,40 @@
 "use client";
 
-import Image from "next/image";
-import todoLabel from "../../public/imgs/todo-label.svg";
-import doneLabel from "../../public/imgs/done-label.svg";
-import CheckList from "../../components/check-list";
 import AddInput from "../../components/add-input";
-import ActionBtn from "../../components/action-btn";
+import { getItems, GetItemsResponse } from "../../services/apis/itemApi";
+import TodoList from "./_components/todo-list";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [todoItems, setTodoItems] = useState<GetItemsResponse[]>([]);
+  const [doneItems, setDoneItems] = useState<GetItemsResponse[]>([]);
+
+  useEffect(() => {
+    getItems().then((items) => {
+      const _todoItems: GetItemsResponse[] = [];
+      const _doneItems: GetItemsResponse[] = [];
+
+      // todo와 done 분리
+      items.map((item) =>
+        !item.isCompleted ? _todoItems.push(item) : _doneItems.push(item),
+      );
+
+      setTodoItems(_todoItems);
+      setDoneItems(_doneItems);
+    });
+  }, []);
+
   return (
     <div className="py-[16px] tablet:py-[24px]">
       {/* 할 일 추가 섹션 */}
       <section className="mb-[24px] flex gap-[8px] tablet:mb-[40px] tablet:gap-[16px]">
         <AddInput />
-        <ActionBtn onClick={() => {}} type="add" />
       </section>
 
       {/* 할 일 리스트 섹션 */}
       <section className="flex flex-col gap-[48px] pc:flex-row pc:gap-[24px]">
-        {/* Todo List*/}
-        <div className="flex grow flex-col gap-[16px]">
-          <Image src={todoLabel} alt="todoLabel" />
-          <CheckList key={1} isDone={true} itemId={1} text="밥 먹기" />
-          <CheckList key={1} isDone={true} itemId={1} text="밥 먹기" />
-          <CheckList key={1} isDone={true} itemId={1} text="밥 먹기" />
-        </div>
-        {/* Done List*/}
-        <div className="flex grow flex-col gap-[16px]">
-          <Image src={doneLabel} alt="doneLabel" />
-          <CheckList key={1} isDone={false} itemId={1} text="밥 먹기" />
-          <CheckList key={1} isDone={false} itemId={1} text="밥 먹기" />
-          <CheckList key={1} isDone={false} itemId={1} text="밥 먹기" />
-        </div>
+        <TodoList items={todoItems} type="todo" /> {/* Todo List*/}
+        <TodoList items={doneItems} type="done" /> {/* Done List*/}
       </section>
     </div>
   );
